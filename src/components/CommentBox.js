740ea805315dc747d37fce518e1da4932d13017a
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-02 20:24:51 +0100

    added app test

---
+++ src/components/CommentBox.js
@@ -0,0 +1,9 @@
+const CommentBox = () => {
+  return (
+    <div className="commentBox">
+      Hello, world! I am a CommentBox.
+    </div>
+  );
+}
+
+export default CommentBox;
---
+++ src/components/CommentList.js
@@ -0,0 +1,10 @@
+const CommentList = ({ comments }) => {
+
+return (
+  <div>
+    <h1>Comment list</h1>
+  </div>
+)
+}
+
+export default CommentList;
