5e433e600818fab15085f3cb089f21acb850f4fa
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-02 20:33:48 +0100

    verify that comment box exists

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,16 +2,15 @@ import React from 'react'
 import ReactDOM  from 'react-dom'
 import App from '../App'
 
-it('renders without crashing', () => {
+it('renders comment box', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
+  expect(div.innerHTML).toContain('Hello, world! I am a CommentBox.')
   ReactDOM.unmountComponentAtNode(div)
 }
 )
 
 it('renders commentBox', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App/>, div);
-  ReactDOM.unmountComponentAtNode(div)
+ 
 }
 )
