4fcbf8787a6e90281982929d13f7f749ff3b1932
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-09 08:28:06 +0100

    added enzyme adapter

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,12 +5,8 @@ import App from '../App'
 it('renders comment box', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
-  expect(div.innerHTML).toContain('Hello, world! I am a CommentBox.')
+ 
   ReactDOM.unmountComponentAtNode(div)
 }
 )
 
-it('renders commentBox', () => {
- 
-}
-)
