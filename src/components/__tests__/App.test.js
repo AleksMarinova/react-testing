be0ec91f1e30f614110f8dfb669f93c2b9ba37a9
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-09 08:41:03 +0100

    added shallow rendering for app

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import ReactDOM  from 'react-dom'
+import { shallow } from 'enzyme';
 import App from '../App'
+import CommentBox from '../CommentBox';
 
 it('renders comment box', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
- 
-  ReactDOM.unmountComponentAtNode(div)
-}
-)
+ const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+})
 
