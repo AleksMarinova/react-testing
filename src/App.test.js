4fcd0c4aed7339251759aae40fefb5166e363944
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-10 10:29:36 +0100

    added simulation of textarea input

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM  from 'react-dom'
-import App from './components/App'
-
-it('should make the error message dissapear', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,3 +17,10 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 })
 
+it('users can enter input into text area', () => {
+  const textarea = wrapped.find('textarea');
+  textarea.simulate('change', {
+    target: { value: 'new comment' }
+  });
+  expect(wrapped.state('comment')).toEqual('new comment');
+})
