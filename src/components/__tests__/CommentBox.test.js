37a082754d7e93734d23849de180c7197358785b
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-10 10:06:31 +0100

    added after each for CommentBox tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,8 +2,18 @@ import CommentBox from 'components/CommentBox';
 import React from 'react';
 import { mount } from 'enzyme';
 
+let wrapped;
+
+beforeEach(() =>{
+wrapped = mount(<CommentBox />);
+})
+
+afterEach(() => {
+  wrapped.unmount();
+})
+
 it('has a text area and a button', () => {
-  const wrapped = mount(<CommentBox />);
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 })
+
